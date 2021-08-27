92fa858f50cb81e2de15a18827a6798583c241c9
Diego <diegopaginidev@gmail.com>
2021-08-27 09:26:09 -0300

    credit card type

--- src/app/components/credit-card/credit-card.component.ts
+++ src/app/components/credit-card/credit-card.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Icon, IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import {
   faCcVisa,
   faCcMastercard,
   faCcAmex,
   faCcDiscover,
 } from '@fortawesome/free-brands-svg-icons';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-credit-card',
   templateUrl: './credit-card.component.html',
   styleUrls: ['./credit-card.component.scss'],
 })
-export class CreditCardComponent implements OnInit {
+export class CreditCardComponent implements OnInit, OnDestroy {
   public creditCards = {
     visa: faCcVisa,
     master: faCcMastercard,
@@ -20,6 +23,8 @@ export class CreditCardComponent implements OnInit {
     discover: faCcDiscover,
   };
   public creditCardForm: FormGroup;
+  public creditCardType: IconDefinition;
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
   constructor(private fb: FormBuilder) {
     this.creditCardForm = this.fb.group({
@@ -34,5 +39,24 @@ export class CreditCardComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.creditCardForm
+      .get('cardNumber')
+      .valueChanges.pipe(filter(Boolean), takeUntil(this.unsubscribe$))
+      .subscribe((cardNumber: number) => {
+        let card = cardNumber.toString();
+        if (card.startsWith('4')) {
+          this.creditCardType = this.creditCards.visa;
+        } else if (card.startsWith('5')) {
+          this.creditCardType = this.creditCards.master;
+        } else if (card.startsWith('3')) {
+          this.creditCardType = this.creditCards.amex;
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
