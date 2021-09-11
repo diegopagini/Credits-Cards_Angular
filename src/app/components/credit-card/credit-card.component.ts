77a857af8b2dd238c791314e151c9bf095687220
Diego Pagini <diegopaginidev@gmail.com>
2021-09-11 16:47:56 +0200

    background credit card

--- src/app/components/credit-card/credit-card.component.ts
+++ src/app/components/credit-card/credit-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Icon, IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import {
   faCcVisa,
   faCcMastercard,
@@ -24,14 +24,29 @@ export class CreditCardComponent implements OnInit, OnDestroy {
   };
   public creditCardForm: FormGroup;
   public creditCardType: IconDefinition;
+  public expirationMonths: any[] = [
+    { name: 'January', value: 1 },
+    { name: 'February', value: 2 },
+    { name: 'March', value: 3 },
+    { name: 'April', value: 4 },
+    { name: 'May', value: 5 },
+    { name: 'June', value: 6 },
+    { name: 'July', value: 7 },
+    { name: 'August', value: 8 },
+    { name: 'September', value: 9 },
+    { name: 'October', value: 10 },
+    { name: 'November', value: 11 },
+    { name: 'December', value: 12 },
+  ];
   private unsubscribe$: Subject<void> = new Subject<void>();
+  public isFront: boolean = true;
 
   constructor(private fb: FormBuilder) {
     this.creditCardForm = this.fb.group({
       cardNumber: ['', [Validators.required, Validators.minLength(16)]],
       cardHolder: ['', [Validators.required, Validators.minLength(3)]],
       cardExpirationYear: ['', [Validators.required]],
-      cardExpirationMonth: ['', [Validators.required]],
+      cardExpirationMonth: ['1', [Validators.required]],
       cardCCV: [
         '',
         [Validators.required, Validators.minLength(3), Validators.maxLength(3)],
@@ -51,8 +66,27 @@ export class CreditCardComponent implements OnInit, OnDestroy {
           this.creditCardType = this.creditCards.master;
         } else if (card.startsWith('3')) {
           this.creditCardType = this.creditCards.amex;
+        } else {
+          this.creditCardType = this.creditCards.discover;
         }
       });
+  }
+
+  public setCreditCardBackground() {
+    let creditCardNumber: any = this.creditCardForm.get('cardNumber').value;
+
+    if (creditCardNumber) {
+      creditCardNumber = creditCardNumber.toString();
+
+      if (
+        creditCardNumber &&
+        !creditCardNumber.startsWith('3') &&
+        !creditCardNumber.startsWith('4') &&
+        !creditCardNumber.startsWith('5')
+      ) {
+        return true;
+      }
+    }
   }
 
   ngOnDestroy(): void {
